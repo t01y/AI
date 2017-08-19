459068c525acf1f0c62a90a397ead69ad6ab45ae
but0n <but0n@icloud.com>
2017-08-19 16:07:38 +0800

    prototype

--- src/core.c
+++ src/core.c
@@ -1,51 +1,83 @@
 #include <stdio.h>
 #include <math.h>
 
-#define MAX_CHILD_NODE	10
+#define MAX_CHILD_NODE	64
 typedef struct node {
-	unsigned char	index;
 	struct			node * child[MAX_CHILD_NODE];
-	float			W[MAX_CHILD_NODE];
+	float			W[MAX_CHILD_NODE]; // Weights
 	float			sig;
+	char	index;
 }st_node, *pst_node;
 
-void computSingleNode(st_node *this) {
-	for (unsigned int i = 0; i < this->index; i++) {
-		this->sig += this->child[i]->sig * this->W[i];
-	}
-	this->sig = (float)tanh(this->sig);
-}
-
 static st_node	nodeStack[256];
-static long		sp = -1;
 
-void test() {
-	// Create some nodes
-	pst_node	node1 = &nodeStack[++sp];
-	pst_node	node2 = &nodeStack[++sp];
-	pst_node	node3 = &nodeStack[++sp];
-
-	// Add childs
-	node1->child[0] = node2;
-	node1->index++;
-	node1->child[1] = node3;
-	node1->index++;
-
-	// Setup
-	node1->W[0] = 1;
-	node1->W[1] = 0.3;
-
-	node2->sig = 1;
-	node3->sig = 1.5;
+void comput() {
+	for (pst_node this = &nodeStack[0]; this->index>=0; this++) {
+		if (this->index) {
+			this->sig = 0; // Clean signal
+			for (unsigned int n = 0; n < this->index; n++) {
+				this->sig += this->child[n]->sig * this->W[n];
+			}
+			this->sig = (float)tanh(this->sig);
+		}
+	}
 }
 
-void console(pst_node n) {
-	printf("\n%d\n%f\n\n", n->index, n->sig);
+// Actually, this function is going to create a sigle layer network
+void createNetwork(
+	unsigned int layer1,
+	unsigned int layer2
+) {
+	for (unsigned int i = layer1; i < layer1+layer2; i++) {
+		for (unsigned int ii = 0; ii < layer1; ii++) {
+			nodeStack[i].child[ii] = &nodeStack[ii];
+			nodeStack[i].index++;
+		}
+	}
+	// set flag
+	nodeStack[layer1+layer2].index = -1;
+
+	// set input source node
+	for (unsigned int i = 0; i < layer1; i++) {
+		nodeStack[i].sig = 0.1;
+	}
 }
 
+void setDNA(float *weights) {
+	for (pst_node this = &nodeStack[0]; this->index>=0; this++) {
+		for (unsigned char i = 0; i < this->index; i++) {
+			this->W[i] = *weights++;
+		}
+	}
+}
+void getDNA(float *weights) {
+	for (pst_node this = &nodeStack[0]; this->index>=0; this++) {
+		for (unsigned char i = 0; i < this->index; i++) {
+			*weights++ = this->W[i];
+		}
+	}
+}
+
+void console(pst_node n, unsigned char times) {
+	for (unsigned char i = 0; i < times; i++,n++) {
+		printf("Node %d - 0x%08x\t%d\t%f\n", i, (unsigned int)n, n->index, n->sig);
+		for (unsigned char i = 0; i < n->index; i++) {
+			printf("%f - ", n->W[i]);
+		}
+		printf("\n");
+	}
+}
+float dna[] = {
+	0.1, 0.2, 0.3, 0.4, 0.5, 0.1, 0.2, 0.3, 0.4, 0.5,
+	0.1, 0.2, 0.3, 0.4, 0.5, 0.1, 0.2, 0.3, 0.4, 0.5,
+	0.1, 0.2, 0.3, 0.4, 0.5, 0.1, 0.2, 0.3, 0.4, 0.5,
+	0.1, 0.2, 0.3, 0.4, 0.5, 0.1, 0.2, 0.3, 0.4, 0.5,
+	0.1, 0.2, 0.3, 0.4, 0.5, 0.1, 0.2, 0.3, 0.4, 0.5,
+};
 int main(int argc, char const *argv[]) {
-	test();
-	computSingleNode(&nodeStack[0]);
-	console(&nodeStack[0]);
+	createNetwork(10, 5);
+	setDNA(dna);
+	comput();
+	console(&nodeStack[0], 15);
 	return 0;
 }
